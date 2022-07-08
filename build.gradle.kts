00d6b352ec86cbfd033b5729ccd77aeadc0deb5b
Fandy Backers <fandy@majiksoft.nl>
2022-07-08 15:04:16 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,13 +52,13 @@ buildscript {
     }
 }
 
-
 fun isNonStable(version: String): Boolean {
     val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
     val regex = "^[0-9,.v-]+(-r)?$".toRegex()
     val isStable = stableKeyword || regex.matches(version)
     return isStable.not()
 }
+
 tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
     rejectVersionIf {
         isNonStable(candidate.version)
@@ -88,5 +88,3 @@ tasks.wrapper {
     //noinspection UnnecessaryQualifiedReference
     distributionType = Wrapper.DistributionType.ALL
 }
-
-
