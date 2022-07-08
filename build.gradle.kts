cea24290f68fd50660d84aa625d2306483f842cd
Fandy Backers <fandy@majiksoft.nl>
2022-07-08 13:53:28 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -83,10 +83,10 @@ jacoco {
 }
 
 
-tasks.test {
+tasks.test() {
     finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
 }
-tasks.jacocoTestReport {
+tasks.jacocoTestReport() {
     dependsOn(tasks.test) // tests are required to run before generating the report
 }
 
